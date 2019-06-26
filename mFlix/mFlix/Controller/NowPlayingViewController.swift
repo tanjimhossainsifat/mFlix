d4a91e2a32eb56cb41619d0c8a060f6b6567d487
Tanjim Hossain <tanjimhossainsifat@gmail.com>
2019-06-26 20:45:19 +0600

    Initial UI created

---
+++ mFlix/mFlix/Controller/NowPlayingViewController.swift
@@ -0,0 +1,30 @@
+//
+//  NowPlayingViewController.swift
+//  mFlix
+//
+//  Created by Tanjim Hossain Sifat on 26/6/19.
+//  Copyright © 2019 Tanjim Hossain Sifat. All rights reserved.
+//
+
+import UIKit
+
+class NowPlayingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
