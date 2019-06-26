84e78c09de08929af67ce28986330c587d22d648
Tanjim Hossain <tanjimhossainsifat@gmail.com>
2019-06-27 01:11:00 +0600

    filter and refreshcontrol added

--- mFlix/mFlix/Controller/NowPlayingViewController.swift
+++ mFlix/mFlix/Controller/NowPlayingViewController.swift
@@ -13,9 +13,16 @@ class NowPlayingViewController: UIViewController{
     @IBOutlet weak var searchBar: UISearchBar!
     @IBOutlet weak var tableView: UITableView!
     
+    var tableViewRefreshControl = UIRefreshControl()
+    
     let movieTableDataSource = MovieTableDataSource()
     let movieApi = MovieDBApi()
     
+    var movies = [Movie]() {
+        didSet {
+            filteredMovies = movies
+        }
+    }
     var filteredMovies = [Movie]() {
         didSet {
             movieTableDataSource.movies = filteredMovies
@@ -25,8 +32,10 @@ class NowPlayingViewController: UIViewController{
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupRefreshControls()
         tableView.dataSource = movieTableDataSource
         tableView.delegate = self
+        searchBar.delegate = self
         fetchDataByApi()
     }
 
@@ -35,17 +44,22 @@ class NowPlayingViewController: UIViewController{
 extension NowPlayingViewController : MovieDBDelegate {
     func didFinishUpdatingMovies(movies: [Movie]) {
         MBProgressHUD.hide(for: self.view, animated: true)
-        self.filteredMovies = movies
+        self.movies = movies
+        DispatchQueue.main.async {
+            self.tableViewRefreshControl.endRefreshing()
+        }
     }
     
     func didFailWithError(error: Error) {
-        
+        DispatchQueue.main.async {
+            self.tableViewRefreshControl.endRefreshing()
+        }
     }
     
 }
 
 extension NowPlayingViewController {
-    func fetchDataByApi() {
+    @objc func fetchDataByApi() {
         MBProgressHUD.showAdded(to: self.view, animated: true)
         movieApi.delegate = self
         movieApi.fetchMovies()
@@ -54,4 +68,28 @@ extension NowPlayingViewController {
 
 extension NowPlayingViewController : UITableViewDelegate {
     
+}
+
+extension NowPlayingViewController: UISearchBarDelegate {
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        filteredMovies = searchText.isEmpty ? movies :  movies.filter {($0.title ?? "").range(of: searchText, options: .caseInsensitive, range: nil, locale: nil) != nil }
+    }
+    
+    func searchBarTextDidBeginEditing(_ searchBar: UISearchBar) {
+        self.searchBar.showsCancelButton = true
+    }
+    
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        searchBar.showsCancelButton = false
+        searchBar.text = ""
+        filteredMovies = movies
+        searchBar.resignFirstResponder()
+    }
+}
+
+extension NowPlayingViewController {
+    func setupRefreshControls() {
+        tableViewRefreshControl.addTarget(self, action: #selector(self.fetchDataByApi), for: .valueChanged)
+        tableView.insertSubview(tableViewRefreshControl, at: 0)
+    }
 }
